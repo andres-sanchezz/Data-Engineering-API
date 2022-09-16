93db6b2f88f27ea048b4f8ab5074ecffc08b78de
Andrés Sánchez Castrillón <ansanch@bancolombia.com.co>
2022-09-15 20:02:49 -0500

    Lambda_Backup_Package developed

---
+++ Lambda_Backup_Package/lambda_function.py
@@ -0,0 +1,113 @@
+import boto3
+import logging
+import pandas as pd
+from fastavro import writer, reader, parse_schema
+from sqlalchemy import create_engine
+import pymysql
+import rds_config
+import sys
+
+s3_client = boto3.client('s3')
+
+rds_host  = "apidb.cisebersdotq.us-east-1.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+    
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+def lambda_handler():    
+    data = pd.read_sql("select * from Company_db.api_hired_employee", conn)
+
+    schema = {
+        'doc': 'BACKUP',
+        'name': 'BACKUP',
+        'namespace': 'company',
+        'type': 'record',
+        'fields': [
+            {'name': 'id', 'type': 'string'},
+            {'name': 'name', 'type': 'string'},
+            {'name': 'timestamp', 'type': 'string'},
+            {'name': 'department_id', 'type': 'int'},
+            {'name': 'job_id', 'type': 'int'}
+        ]
+    }
+
+    parsed_schema = parse_schema(schema)
+
+    records = data.to_dict('records')
+
+    with open('hired_employee.avro', 'wb') as out:
+        writer(out, parsed_schema, records)
+
+    s3_client.put_object(
+        Body='hired_employee.avro',
+        Bucket='backup-compnay-bucket', 
+        Key='hired_employee.avro'
+    )
+
+    data = pd.read_sql("select * from Company_db.api_departments", conn)
+
+    schema = {
+        'doc': 'BACKUP',
+        'name': 'BACKUP',
+        'namespace': 'company',
+        'type': 'record',
+        'fields': [
+            {'name': 'id', 'type': 'string'},
+            {'name': 'department', 'type': 'string'}
+        ]
+    }
+
+    parsed_schema = parse_schema(schema)
+
+    records = data.to_dict('records')
+
+    with open('departments.avro', 'wb') as out:
+        writer(out, parsed_schema, records)
+
+    s3_client.put_object(
+        Body='departments.avro',
+        Bucket='backup-compnay-bucket', 
+        Key='departments.avro'
+    )
+
+    data = pd.read_sql("select * from Company_db.api_jobs", conn)
+
+    schema = {
+        'doc': 'BACKUP',
+        'name': 'BACKUP',
+        'namespace': 'company',
+        'type': 'record',
+        'fields': [
+            {'name': 'id', 'type': 'string'},
+            {'name': 'job', 'type': 'string'}
+        ]
+    }
+
+    parsed_schema = parse_schema(schema)
+
+    records = data.to_dict('records')
+
+    with open('jobs.avro', 'wb') as out:
+        writer(out, parsed_schema, records)
+
+    s3_client.put_object(
+        Body='jobs.avro',
+        Bucket='backup-compnay-bucket', 
+        Key='jobs.avro'
+    )
+
+    return "Backup items from RDS MySQL table"
+
+print(lambda_handler())
